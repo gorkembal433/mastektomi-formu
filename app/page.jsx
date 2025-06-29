2b0aa1be91dc3225e349a0d52f1cc37356cbee7c
gorkembal433 <48773006+gorkembal433@users.noreply.github.com>
2025-06-30 00:20:35 +0300

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+import BreastMapForm from "@/components/BreastMapForm";
+
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Mastektomi Makroskopi Şablonu</h1>
+      <BreastMapForm />
+    </main>
+  );
+}
